bbf281f3eb62679e80b073afe71f1b7cfa551972
Olli Hihnala <39198545+korintic@users.noreply.github.com>
2018-05-29 13:21:55 +0300

    Updated SnapshotMesh to support keeping the rig.

--- Editor/SnapshotMesh.cs
+++ Editor/SnapshotMesh.cs
@@ -1,8 +1,21 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor;
+using System.Collections;
+using System;
 
 public class SnapshotMesh : EditorWindow
 {
+
+    private bool _keepRig;
+    private bool _preferenceFoldout;
+    // private bool _keepMesh;
+    // private bool _keepAnimator;
+
+    private void Awake()
+    {
+        _keepRig = false;
+    }
+
     [MenuItem("Tools/Snapshot Mesh")]
     public static void ShowWindow()
     {
@@ -14,6 +27,11 @@ public class SnapshotMesh : EditorWindow
         EditorGUILayout.LabelField("Utility for taking snapshots of skinned meshes.");
         EditorGUILayout.Space();
 
+        EditorGUIUtility.labelWidth = Screen.width / 3.5f;
+        _keepRig = EditorGUILayout.Toggle("Keep rig:", _keepRig);
+        EditorGUIUtility.labelWidth = 0;
+
+        EditorGUILayout.Space();
         GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
         if (GUILayout.Button("Take Snapshot", GUILayout.Width(Screen.width / 2), GUILayout.Height(40)))
@@ -23,41 +41,129 @@ public class SnapshotMesh : EditorWindow
             {
                 for(int i = 0; i < Selection.transforms.Length; i++)
                 {
-                    if (Selection.gameObjects[i].GetComponent<SkinnedMeshRenderer>())
+                    if (Selection.activeGameObject.GetComponentsInChildren<SkinnedMeshRenderer>() != null)
                     {
-                        SkinnedMeshRenderer _sk;
-                        GameObject _clone;
-                        Mesh _snapshotMesh = new Mesh();
-                        _sk = Selection.gameObjects[i].GetComponent<SkinnedMeshRenderer>();
-                        _sk.BakeMesh(_snapshotMesh);
-
-                        _clone = Instantiate(Selection.gameObjects[i], null);
-                        _clone.transform.position = _sk.GetComponent<Transform>().position;
-                        _clone.transform.rotation = _sk.GetComponent<Transform>().rotation;
-                        _clone.transform.localScale = _sk.GetComponent<Transform>().localScale;
-
-                        SkinnedMeshRenderer _destroyThis = _clone.GetComponent<SkinnedMeshRenderer>();
-                        DestroyImmediate(_destroyThis);
-                        _clone.AddComponent<MeshFilter>();
-                        _clone.AddComponent<MeshRenderer>();
-                        _clone.GetComponent<MeshFilter>().sharedMesh = _snapshotMesh;
-                        _clone.GetComponent<MeshRenderer>().sharedMaterial = _sk.GetComponent<SkinnedMeshRenderer>().sharedMaterial;
-                        _clone.name = _clone.name.ToString().Replace("(Clone)", "_SnapShot");
+                        if (!_keepRig)
+                        {
+                            CaptureMesh(i);
+                        }
+                        else
+                        {
+                            CloneWithRig(i);
+                        }
                     }
                     else
                     {
-                        EditorUtility.DisplayDialog("Invalid Selection", "Selection needs to have at least one GameObject with Skinned Mesh Renderer component!", "OK");
+                        EditorUtility.DisplayDialog("Invalid Selection", "Selection needs to have at least one GameObject with SkinnedMeshRenderer component!", "OK");
                     }
                 }
             }
  
             else
             {
-                EditorUtility.DisplayDialog("Invalid Selection", "Nothing selected!\n\nSelect GameObject(s) with a Skinned Mesh Renderer component.", "OK");
+                EditorUtility.DisplayDialog("Invalid Selection", "Nothing selected!\n\nSelect GameObject(s) with a SkinnedMeshRenderer component.", "OK");
             }
         }
 
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+
+        //These are not yet implemented!
+        // _preferenceFoldout = EditorGUILayout.Foldout(_preferenceFoldout, "Preferences:");
+        // if (_preferenceFoldout)
+        // {
+        //     _keepMesh = EditorGUILayout.Toggle("Save created mesh:", _keepMesh);
+
+        //     EditorGUILayout.LabelField("Save mesh to project folder:");
+        //     EditorGUILayout.BeginHorizontal();
+        //     EditorGUIUtility.labelWidth = 0.1f;
+        //     GUI.enabled = false;
+        //     EditorGUILayout.LabelField("Assets/");
+        //     EditorGUIUtility.labelWidth = 0f;
+        //     GUI.enabled = true;
+        //     EditorGUILayout.TextField("");
+        //     EditorGUILayout.EndHorizontal();
+
+        //     _keepAnimator = EditorGUILayout.Toggle("Save created animator:", _keepAnimator);
+
+        //     EditorGUILayout.LabelField("Save animator to project folder:");
+        //     EditorGUILayout.BeginHorizontal();
+        //     EditorGUIUtility.labelWidth = 0.1f;
+        //     GUI.enabled = false;
+        //     EditorGUILayout.LabelField("Assets/");
+        //     EditorGUIUtility.labelWidth = 0f;
+        //     GUI.enabled = true;
+        //     EditorGUILayout.TextField("");
+        //     EditorGUILayout.EndHorizontal();
+
+        // }
+    }
+
+    private void CaptureMesh(int i)
+    {
+        if (Selection.activeGameObject.GetComponent<SkinnedMeshRenderer>() != null)
+        {
+            SkinnedMeshRenderer sk;
+            Mesh snapshotMesh = new Mesh();
+
+            sk = Selection.gameObjects[i].GetComponent<SkinnedMeshRenderer>();
+            sk.BakeMesh(snapshotMesh);
+
+            string fileName = ConstructName();
+
+            GameObject _newSnapshot = new GameObject(fileName);
+            _newSnapshot.transform.position = Selection.gameObjects[i].GetComponent<Transform>().position;
+            _newSnapshot.transform.rotation = Selection.gameObjects[i].GetComponent<Transform>().rotation;
+            _newSnapshot.transform.localScale = Selection.gameObjects[i].GetComponent<Transform>().localScale;
+
+            _newSnapshot.AddComponent<MeshFilter>();
+            _newSnapshot.AddComponent<MeshRenderer>();
+            _newSnapshot.GetComponent<MeshFilter>().sharedMesh = snapshotMesh;
+            _newSnapshot.GetComponent<MeshRenderer>().sharedMaterial = sk.GetComponent<SkinnedMeshRenderer>().sharedMaterial;
+            AssetDatabase.CreateAsset(snapshotMesh, "Assets/"+fileName+".asset");
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Invalid Selection", "Selection needs to have at least one GameObject with SkinnedMeshRenderer component!", "OK");
+        }
+
+    }
+
+    private void CloneWithRig(int i)
+    {
+        Mesh snapshotMesh = new Mesh();
+        GameObject clone;
+        clone = Instantiate(Selection.transforms[i].gameObject, null);
+
+        Component[] components = clone.GetComponentsInChildren<Component>(true);
+        
+        for (int j = 0; j < components.Length; j++)
+        {
+            ArrayList excludedComponents = new ArrayList() { typeof(Transform), typeof(SkinnedMeshRenderer), typeof(MeshFilter), typeof(MeshRenderer) };
+        
+            if (!excludedComponents.Contains(components[j].GetType()))
+            {
+                DestroyImmediate(components[j]);
+            }
+        }
+        string fileName = ConstructName();
+        clone.name = clone.name.ToString().Replace("(Clone)", fileName);
+
+    }
+
+    private Animator CreateAnimator()
+    {
+        return new Animator();
+    }
+
+    private string ConstructName()
+    {
+        string dateString;
+        DateTime dateTime = DateTime.Now;
+        dateString = dateTime.ToString("yyMMddHHmmss");
+        string fileName = "Snapshot_" + dateString;
+
+        return fileName;
     }
 }
